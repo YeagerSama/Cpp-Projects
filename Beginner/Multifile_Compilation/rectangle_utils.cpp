0fc8385bc97e0564c203fcebfd414072771f0c0c
YeagerSama <158847637+YeagerSama@users.noreply.github.com>
2025-08-02 18:47:58 +0530

    Create rectangle_utils.cpp

---
+++ Beginner/Multifile_Compilation/rectangle_utils.cpp
@@ -0,0 +1,11 @@
+#include "rectangles_utils.h"
+
+double calculateArea(double length, double width){
+
+    return length*width;
+}
+
+double calculatePerimeter(double length, double width){
+
+    return 2*(length+width);
+}
